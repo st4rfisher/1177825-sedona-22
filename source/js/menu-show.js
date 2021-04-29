7ecc5532ecd2482ab683fdc20ff3e6274dc8d998
Starfisher <supremeeworker.com>
2021-04-29 17:48:06 +0300

    Добавление ссылок в футере, декоративные элементы при наведении и нажатии, реализация мобильного меню, добавление js-файла для отображения мобильного меню

---
+++ source/js/menu-show.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+const menuMobile = document.querySelector('.menu--mobile');
+const item = document.querySelector('.menu__item');
+const itemMobile = document.querySelector('.menu__item--mobile');
+const openToggle = document.querySelector('.navigation__toggle--open');
+const closeToggle = document.querySelector('.navigation__toggle--close');
+
+let 
