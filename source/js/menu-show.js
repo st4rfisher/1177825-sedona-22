048d10c7a62956ee4022658940e46c50ad944f30
Starfisher <vladokforever@yandex.ru>
2021-04-30 22:45:32 +0300

    Подправил блок навигации в планшетной версии

--- source/js/menu-show.js
+++ source/js/menu-show.js
@@ -1,5 +1,5 @@
-const menu = document.querySelector('.menu');
-const menuJS = document.querySelector('.menu--js');
+const navigation = document.querySelector('.navigation');
+const navigationActive = document.querySelector('.navigation--active');
 const item = document.querySelector('.menu__item');
 const openButton = document.querySelector('.navigation__toggle--open');
 const closeButton = document.querySelector('.navigation__toggle--close');
