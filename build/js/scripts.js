799c7e5c816bac31bc4dba25809eae2337e8435c
Starfisher <supremeeworker@gmail.com>
2021-10-12 21:44:28 +0300

    feat: исключение папки build из gitignore

---
+++ build/js/scripts.js
@@ -0,0 +1,26 @@
+const navigation = document.querySelector('.navigation');
+const openButton = document.querySelector('.header__toggle');
+const closeButton = navigation.querySelector('.navigation__close-button');
+
+const hideNavigation = () => {
+  closeButton.classList.remove('navigation__close-button--show');
+  navigation.classList.remove('navigation--active');
+}
+
+const showNavigation = () => {
+  closeButton.classList.add('navigation__close-button--show');
+  navigation.classList.add('navigation--active');
+}
+
+const hideNoJsNavigation = () => {
+  navigation.classList.remove('navigation--no-js');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  openButton.classList.add('header__toggle--show');
+  hideNoJsNavigation();
+  hideNavigation();
+});
+
+openButton.addEventListener('click', showNavigation);
+closeButton.addEventListener('click', hideNavigation);
